0d5c6627ab9e511afb8b75fc24ec3538e1ad2330
Christian Yves Dubuche <dubuche@gmail.com>
2018-05-11 12:53:41 -0400

    Unit Tests

---
+++ Dubuche.TEST/RestaurantControllerTEST.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Restaurants.MVC.Controllers;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using Dubuche.DAL;
+
+namespace Dubuche.TEST
+{
+    [TestClass]
+    public class RestaurantControllerTEST
+    {
+        [TestMethod]
+        public void TestRestaurantsIndex()
+        {
+            //Arrange
+            RestaurantController controller = new RestaurantController();
+            string e1 = "Mission BBQ";
+            
+
+            //Act
+            var result1 = controller.Index() as ViewResult;
+            var data1 = result1.Model as List<Restaurant>;
+           
+
+            //Assert
+            Assert.IsNotNull(result1);
+            Assert.AreEqual(e1, data1[0].Name);
+           
+        }
+    }
+}
---
+++ Dubuche.TEST/ReviewerControllerTEST.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dubuche.TEST
+{
+    [TestClass]
+    public class ReviewerControllerTEST
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
